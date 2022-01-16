ae3dafa942a877508a285e8f7cafca91279b9302
Marek <mar94bor@gmail.com>
2022-01-16 12:27:19 +0100

    State machine created

---
+++ Knife Hit Remake/Assets/Scripts/SDA.Architecture/StateMachine/States/MenuState.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace SDA.Architecture
+{
+    public class MenuState : BaseState
+    {
+        public override void InitState()
+        {
+            Debug.Log("init menu");
+        }
+
+        public override void UpdateState()
+        {
+
+        }
+
+        public override void DestroyState()
+        {
+
+        }
+    }
+}
